5e22f189fa7934df72d6fae115e40975bbca52be
Lowace <87303949+Lowace@users.noreply.github.com>
2024-04-14 15:18:45 +0800

    Add files via upload

---
+++ WEB YOMIE/script.js
@@ -0,0 +1,8 @@
+// Parallax effect
+window.addEventListener('scroll', function() {
+    const parallax = document.querySelector('.parallax');
+    let scrollPosition = window.pageYOffset;
+  
+    parallax.style.backgroundPositionY = scrollPosition * 0.7 + 'px';
+  });
+  
